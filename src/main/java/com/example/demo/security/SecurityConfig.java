738e1bf4465e0a0eb5970b71c8ac36be5ba09f08
diasha003 <fursevichdiana42@gmail.com>
2023-08-09 00:05:25 +0300

    сonfigures a security filter chain

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -39,6 +40,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
-        
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/user/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults());
+
+
+        return http.build();
     }
 }
